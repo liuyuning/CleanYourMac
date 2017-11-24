d970166a84c04cce5a098098a47df13137417b5e
liuyuning <liuyuning@LYN-iMac.local>
2017-11-24 18:27:19 +0800

    Add CleanYourMac.sh and log image

---
+++ CleanYourMac.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# CleanYourMac.sh or DeleteCacheFile.sh
+# Created by liuyuning on 2015/10/16.
+# Copyright © 2015-2017 liuyuning. All rights reserved.
+
+INFO_BEFORE=$(df -lh)
+
+mv ~/Library/Developer/Xcode/UserData/FontAndColorThemes ~/
+
+sudo rm -rf /private/var/folders/*
+sudo rm -rf /Library/Developer/ ~/Library/Developer/
+sudo rm -rf /Library/Caches/ ~/Library/Caches/
+sudo rm -rf /Library/Logs/ ~/Library/Logs/ /var/log/
+sudo rm -rf ~/Library/iTunes
+
+sudo rm -rf /Library/SystemMigration/
+sudo rm -rf  /private/var/tmp/
+
+
+sudo rm -rf /System/Library/Caches/com.apple.coresymbolicationd
+
+#apache log dir
+sudo mkdir  /var/log/apache2/
+
+mkdir -p  ~/Library/Developer/Xcode/UserData/
+mv ~/FontAndColorThemes ~/Library/Developer/Xcode/UserData/
+
+#GarageBand 音乐创作
+#https://www.tekrevue.com/tip/delete-garageband/
+sudo rm -r "/Applications/GarageBand.app" #(1.16GB)
+sudo rm -r "/Library/Application Support/GarageBand" #(995MB)
+sudo rm -r "/Library/Application Support/Logic" #(880MB)
+sudo rm -r "/Library/Audio/Apple Loops" #(up to 10GB)*
+
+#安装Xcode插件
+sudo installer -verbose -pkg /Applications/Xcode.app/Contents/Resources/Packages/XcodeSystemResources.pkg -target /
+
+echo "==== BEFORE ===="
+echo "${INFO_BEFORE}"
+echo "==== AFTER ===="
+df -lh
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # CleanYourMac
 Run a script to clean your mac
+
+ℹ️Will remove Xcode downloaded Simulator and symbol of iOS Device.
+
+ℹ️Some dir or file can not remove under root!
+
+#### 1. Usage:
+
+```shell
+$sh CleanYourMac.sh
+```
+
+Part of log:
+![](log.jpg)
