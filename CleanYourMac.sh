19346996cabc870b6f73700d3df0216eb64b4717
liuyuning <liuyuning@LYN-iMac.local>
2017-12-06 15:50:21 +0800

    Fix Asset Catalog Compiler Error

--- CleanYourMac.sh
+++ CleanYourMac.sh
@@ -23,6 +23,16 @@ sudo rm -rf /System/Library/Caches/com.apple.coresymbolicationd
 #apache log dir
 sudo mkdir  /var/log/apache2/
 
+#Asset Catalog Compiler Error
+#Failed to find a suitable device for the type IBSimDeviceTypeiPad2x(com.apple.dt.Xcode.IBSimDeviceType.iPad-2x) with runtime iOS 11.2 (11.2 - 15C107) - com.apple.CoreSimulator.SimRuntime.iOS-11-2
+#Assets.xcassets
+#Asset Catalog Compiler Error Group
+#.../Assets.xcassets: Failed to launch IBCocoaTouchImageCatalogTool via CoreSimulator spawn
+#Interface Builder Storyboard Compiler Error Group
+#.../Base.lproj/LaunchScreen.storyboard: Internal error. Please file a bug at bugreport.apple.com and attach "/var/folders/5f/0s8jnpp513l9y48q4z1thx6m0000gn/T/IB-agent-diagnostics_2017-12-06_15-40-01_656000".
+sudo mkdir ~/Library/Logs/
+sudo chmod 755 ~/Library/Logs/
+
 mkdir -p  ~/Library/Developer/Xcode/
 mv ~/UserData ~/Library/Developer/Xcode/
 
@@ -40,3 +50,5 @@ echo "==== BEFORE ===="
 echo "${INFO_BEFORE}"
 echo "==== AFTER ===="
 df -lh
+
+
